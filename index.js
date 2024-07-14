abc24556cc1977a1eeaac12f2b5404b3b9e1e6fb
creepybrow <144290004+creepybrow@users.noreply.github.com>
2024-07-14 09:38:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 // public key hnP2n1Q8doU5v-nbH
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for(let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+
+}
+
 function toggleContrast(){
     contrastToggle = !contrastToggle;
     if(contrastToggle){
@@ -42,3 +56,4 @@ function toggleModal(){
     //toggle modal
     document.body.classList += " modal--open";
 }
+
